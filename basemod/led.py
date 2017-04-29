b5c8a8a4c6c3cad062755e8737b39fd73a4bc6ec
Hilaly <hilalyamine@gmail.com>
2017-04-29 23:52:49 +0200

    Update led.py

--- basemod/led.py
+++ basemod/led.py
@@ -1,1 +1,31 @@
+from imputil import rgb
+from imputil import rgb
 
+class LED():
+    def __init__(self):
+        self.Name = ""
+        self.Pin = 0
+        self.Cstate = False
+        self.Stategraph
+    def _record(self):
+        pass
+    def _srecord(self):
+        pass
+    def _get_state(self):
+        return self.Cstate = False
+    def _turn_on(self):
+        pass
+    def _turn_off(self):
+        pass
+    def _activate(self):
+        pass
+    def _desactivate(self):
+        pass
+    
+class CLRLED(LED):
+    def __init__(self):
+        LED.__init__(self)
+        self.CLR = rgb.RGB
+    def _show_color(self, rgb):
+        pass
+    def _read_imprgbcode(self, imprgbcode)
