e9aeb9c571c18ccad4d38c619c52fc94225ded75
Hilaly <hilalyamine@gmail.com>
2017-05-11 19:29:05 +0200

    Update basic.py

--- core/basic.py
+++ core/basic.py
@@ -1,1 +1,29 @@
 
+from .core import BasicMeta, with_metaclass
+
+class Basic(with_metaclass(BasicMeta)):
+    is_ElectronicComposent = False
+    is_Solid = False
+    
+    __slots__ = ['_name', '_pins', '_args']
+    
+    def __new__(cls, *args):
+        obj = object.__new__(cls)
+        obj._mhash = None  # will be set by __hash__ method.
+        obj._args = args  # all items in args must be Basic objects
+        return obj
+    
+    def __eq__(self, other):
+        pass
+    
+    def __ne__(self, other):
+        pass
+    
+    def __str__(self):
+        pass
+    
+    __repr__ = __str__
+    
+    
+    
+    
